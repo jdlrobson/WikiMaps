7c7d583850f72a1fbee08378c993abe9ff2f2941
jrobson <jrobson@wikimedia.org>
2014-05-09 15:54:54 +0200

    Default to 0,0 fully zoomed out

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,5 +1,5 @@
 ( function() {
-	var map = L.map( 'mw-wiki-map-main' ).setView( [ 51.505, -0.09 ], 13 );
+	var map = L.map( 'mw-wiki-map-main' ).setView( [ 0, 0 ], 1 );
 	L.tileLayer( mw.config.get( 'extWikiMapsTitleServer' ), {
 		attribution: mw.config.get( 'extWikiMapsAttribution' ),
 		maxZoom: 18
