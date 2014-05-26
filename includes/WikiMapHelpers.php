c5d522a60fe6e6ccc34decb22a057096f6c539b3
jkan997 <jakub.kaniewsky@gmail.com>
2014-05-27 00:42:07 +0200

    SVG maps integration
    * SVG renderer added

--- includes/WikiMapHelpers.php
+++ includes/WikiMapHelpers.php
@@ -1,5 +1,9 @@
 <?php
+
+use ShareMapPhp\SVGRenderer as SVGRenderer;
+
 class WikiMapHelpers {
+
 	/**
 	 * Get the config variables needed by JavaScript to render a map.
 	 * @return array of config variables to set in mw.config
@@ -44,45 +48,12 @@ class WikiMapHelpers {
 
 		if ( $data ) {
 			$data = json_encode( $data );
-			$attrs['data-map'] = $data;
+			$attrs[ 'data-map' ] = $data;
 		}
 
 		return Html::element( 'div',
 			$attrs
 		);
-	}
-	/**
-	 * Probably needs linktable update
-	 * <map title="Map:MyMap" />
-	 * @param $input
-	 * @param array $args array of options
-	 *              title: Find the map at the given title and render it
-	 *              position: Where the map should be located. Omit to make it full screen [left|right]
-	 * @param Parser $parser
-	 * @param PPFrame $frame
-	 * @return string HTML representation of map
-	 */
-	public static function embedMapTag( $input, array $args, Parser $parser, PPFrame $frame ) {
-		$className = isset( $args['class'] ) ? $args['class'] : '';
-		if ( isset( $args['position'] ) ) {
-			$pos = $args['position'];
-			if ( $pos === 'left' ) {
-				$className .= ' side-map side-map-left';
-			} else if ( $pos === 'right') {
-				$className .= ' side-map side-map-right';
-			}
-		}
-		if ( isset( $args['title'] ) ) {
-			$title = Title::newFromText( $args['title'], NS_MAP );
-			$out = $parser->getOutput();
-			$out->addJsConfigVars( self::getSkinConfigVariables() );
-			$out->addModuleStyles( 'wikimaps.styles' );
-			$out->addModules( 'wikimaps.view.scripts' );
-
-			return self::getMapHtmlFromTitle( $title, $className );
-		} else {
-			return self::getMapHtml( array(), $className );
-		}
 	}
 
 	/**
@@ -112,8 +83,8 @@ class WikiMapHelpers {
 	public static function makeGeoJSONFromRequest( $request ) {
 		$features = array();
 		$vals = $request->getValues();
-		$vals['format'] = 'json';
-		$vals['action'] = 'query';
+		$vals[ 'format' ] = 'json';
+		$vals[ 'action' ] = 'query';
 		$api = new ApiMain(
 			new DerivativeRequest( $request, $vals )
 		);
@@ -122,18 +93,18 @@ class WikiMapHelpers {
 		$result = $api->getResult()->getData();
 
 		// FIXME: What if query is not format=json ?
-		if ( isset( $result['query'] ) && isset( $result['query']['pages'] ) ) {
-			$pages = $result['query']['pages'];
+		if ( isset( $result[ 'query' ] ) && isset( $result[ 'query' ][ 'pages' ] ) ) {
+			$pages = $result[ 'query' ][ 'pages' ];
 
-			foreach( $pages as $page ) {
-				if ( isset( $page['coordinates'] ) ) {
+			foreach ( $pages as $page ) {
+				if ( isset( $page[ 'coordinates' ] ) ) {
 					$props = array();
-					if ( isset( $page['title'] ) ) {
-						$props['name'] = $page['title'];
+					if ( isset( $page[ 'title' ] ) ) {
+						$props[ 'name' ] = $page[ 'title' ];
 					}
-					$point_coords = $page['coordinates'];
-					foreach( $point_coords as $coord ) {
-						$coords = array( $coord['lon'], $coord['lat'] );
+					$point_coords = $page[ 'coordinates' ];
+					foreach ( $point_coords as $coord ) {
+						$coords = array( $coord[ 'lon' ], $coord[ 'lat' ] );
 						$features[] = WikiMapHelpers::createFeature( $coords, $props );
 					}
 				}
@@ -145,5 +116,92 @@ class WikiMapHelpers {
 			"features" => $features,
 		);
 		return $data;
+	}
+
+	/**
+	 * Probably needs linktable update
+	 * <map title="Map:MyMap" type="interactive|static" position="left|right" staticwidth="" staticheight=""/>
+	 * @param $input
+	 * @param array $args array of options
+	 *              title: Find the map at the given title and render it
+	 *              type: Interactive or Static map (default: interactive) [interactive|static]
+	 *              width: Width of map box in pixels
+	 *              height: Height of map box in pixels
+	 *              fit: box|extend, in box mode rendered image has to fit inside the box but
+	 *                can have on dimension smaller, in extend mode renderer try to fit entire box, even with empty space
+	 *              position: Where the map should be located. Omit to make it full screen [left|right]
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return string HTML representation of map
+	 */
+	public static function embedMapTag($input, array $args, Parser $parser, PPFrame $frame) {
+		if ( isset( $args['type'] ) ) {
+			if ( $args['type'] === "static" ) {
+				return self::embedStaticMapTag( $input, $args, $parser, $frame );
+			}
+		}
+		return self::embedInteractiveMap( $input, $args, $parser, $frame );
+	}
+
+	/**
+	 * @param $input
+	 * @param array $args array of options
+	 *              title: Find the map at the given title and render it
+	 *              position: Where the map should be located. Omit to make it full screen [left|right]
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return string HTML representation of map
+	 *
+	 * TODO: CC-SA attribution have to handled either by including in SVG or overlaying HTML attribution over SVG
+	 */
+	public static function embedInteractiveMap( $input, array $args, Parser $parser, PPFrame $frame ) {
+		$className = isset( $args[ 'class' ] ) ? $args[ 'class' ] : '';
+		if ( isset( $args[ 'position' ] ) ) {
+			$pos = $args[ 'position' ];
+			if ( $pos === 'left' ) {
+				$className .= ' side-map side-map-left';
+			} else if ( $pos === 'right') {
+				$className .= ' side-map side-map-right';
+			}
+		}
+		if ( isset( $args[ 'title' ] ) ) {
+			$title = Title::newFromText( $args['title'], NS_MAP );
+			$out = $parser->getOutput();
+			$out->addJsConfigVars( self::getSkinConfigVariables() );
+			$out->addModuleStyles( 'wikimaps.styles' );
+			$out->addModules( 'wikimaps.view.scripts' );
+
+			return self::getMapHtmlFromTitle( $title, $className );
+		} else {
+			return self::getMapHtml( array(), $className );
+		}
+	}
+
+	/**
+	 * @param $input
+	 * @param array $args array of options
+	 *  title: Find the map at the given title and render it
+	 *  staticwidth: Width of map in pixels - only used in case of static map, this is maximum dimension, smaller can be in output because of viewport proportions
+	 *  staticheight: Width of map in pixels - only used in case of static map, this is maximum dimension, smaller can be in output because of viewport proportions
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return string HTML representation of map
+	 */
+	public static function embedStaticMapTag($input, array $args, Parser $parser, PPFrame $frame) {
+		$parser->disableCache();
+		$title = Title::newFromText($args['title'], NS_MAP);
+		$out = $parser->getOutput();
+		$page = WikiPage::factory($title);
+		$content = $page->getContent();
+		$data = $content->getJsonData();
+		$svgRenderer = new SVGRenderer();
+		global $wgWikiMapsTileServer;
+		$svgRenderer->tileUrlPattern = $wgWikiMapsTileServer;
+		$svgRenderer->viewportWidth = isset($args['staticwidth'])?$args['staticwidth']:800;
+		$svgRenderer->viewportHeight = isset($args['staticheight'])?$args['staticheight']:600;
+		// For testing it is better to turn this off, in case of caching or rasterizing image embeding is suggested
+		$svgRenderer->embedImg = false;
+		$svgStr = $svgRenderer->renderSVG($data);
+		return array($svgStr, "markerType" => 'nowiki');
 	}
 }
